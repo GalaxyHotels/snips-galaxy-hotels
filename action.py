eaa1c8521c853c4cc638623fc85a81f885c8e593
Galaxy Hotels <contact@galaxy-hotels.fr>
2019-01-15 18:42:43 +0100

    Commit message

--- action.py
+++ action.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import requests 
+
 url = 'https://galaxy-manager.fr/_ajax/snips.php'
+
 payload = {"intentMessage": intentMessage, 'conf' : conf}
+
 headers = {'Content-type': 'application/json'}
+
 res = requests.post(url, data=payload, headers=headers)
 
